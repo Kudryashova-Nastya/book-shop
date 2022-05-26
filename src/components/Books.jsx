4501c7904df8d12c3ea118b548147bdf593a5eee
Kudryashova-Nastya <avk2001avk@gmail.com>
2022-05-27 01:36:18 +0300

    the list of books is displayed

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { observer } from "mobx-react";
+import Store from "../store/Store";
 
-const Books = () => {
+const Books = observer(() => {
+    useEffect(() =>{
+        Store.fetchBooksInfo({})
+    },[])
     return (
         <>
+            {Store.booksInfo !== null ? Store.booksInfo.map((item) => {
+                return <p>{item.name}</p>
+            }) : <p>книг нет</p>}
         </>
     );
-};
+});
 
 export default Books;
---
+++ src/store/Store.js
@@ -0,0 +1,28 @@
+import {makeAutoObservable, runInAction} from "mobx";
+import {getHostInformation, POSTCORS} from "./helper/getHostInformation";
+
+const host = getHostInformation();
+
+class Store {
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    booksInfo = null;
+
+    fetchBooksInfo = async (filters) => {
+        const data = {
+            "filters": filters
+        }
+        const petReq = await fetch(`${host}/bookstore-api/books`, POSTCORS(data));
+        const petRes = await petReq.json();
+        console.log(petRes);
+        if (petReq.ok) {
+            runInAction(() => {
+                this.booksInfo = petRes
+            });
+        }
+    };
+}
+
+export default new Store();
