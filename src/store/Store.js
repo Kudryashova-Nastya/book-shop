5eac0fee1dc11dfde829166bc7e6309d9f1a5364
Kudryashova_Nastya <avk2001avk@gmail.com>
2022-09-08 02:31:53 +0300

    if the server is not responding

--- src/store/Store.js
+++ src/store/Store.js
@@ -71,14 +71,155 @@ class Store {
         const data = {
             "filters": this.filters
         }
-        const petReq = await fetch(`${host}/bookstore-api/books`, POSTCORS(data));
-        const petRes = await petReq.json();
-        // console.log(petRes);
-        if (petReq.ok) {
+        try {
+            const petReq = await fetch(`${host}/bookstore-api/books`, POSTCORS(data));
+            const petRes = await petReq.json();
+            // console.log(petRes);
+            if (petReq.ok) {
+                runInAction(() => {
+                    this.booksInfo = petRes
+                })
+            }
+        } catch {
             runInAction(() => {
-                this.booksInfo = petRes
+                this.booksInfo = [
+                    {
+                        "name": "Отзывчивый веб-дизайн",
+                        "authorName": "Итан Маркотт",
+                        "price": 349,
+                        "coverUrl": "https://cv7.litres.ru/pub/c/elektronnaya-kniga/cover_max1500/3744275-itan-markott-otzyvchivyy-veb-dizayn.jpg",
+                        "categoryId": 1
+                    },
+                    {
+                        "name": "Эмоциональный веб-дизайн",
+                        "authorName": "Аарон Уолтер",
+                        "price": 349,
+                        "coverUrl": "https://cv3.litres.ru/pub/c/elektronnaya-kniga/cover_max1500/3744235-aaron-uolter-emocionalnyy-veb-dizayn.jpg",
+                        "categoryId": 1
+                    },
+                    {
+                        "name": "Дизайн — это работа",
+                        "authorName": "Майк Монтейро",
+                        "price": 349,
+                        "coverUrl": "https://miro.medium.com/max/1047/1*9f2tnp19YvaMotONlONeJg.jpeg",
+                        "categoryId": 1
+                    },
+                    {
+                        "name": "Управление проектами от А до Я",
+                        "authorName": "Ричард Ньютон",
+                        "price": 675,
+                        "coverUrl": "https://cdn1.ozone.ru/s3/multimedia-u/6083509554.jpg",
+                        "categoryId": 2
+                    },
+                    {
+                        "name": "Чистый код: создание, анализ и рефакторинг",
+                        "authorName": "Роберт Мартин",
+                        "price": 700,
+                        "coverUrl": "https://mywishboard.com/thumbs/wish/m/q/t/600x0_dslypurveyhxiige_jpg_0e28.jpg",
+                        "categoryId": 3
+                    },
+                    {
+                        "name": "Бизнес с нуля",
+                        "authorName": "Эрик Рис",
+                        "price": 920,
+                        "coverUrl": "https://cv5.litres.ru/pub/c/elektronnaya-kniga/cover_max1500/6884055-erik-ris-biznes-s-nulya-metod-lean-startup-dlya-bystrogo-testirovaniya-ide.jpg",
+                        "categoryId": 2
+                    },
+                    {
+                        "name": "Идеальный программист",
+                        "authorName": "Роберт Мартин",
+                        "price": 1020,
+                        "coverUrl": "https://knigamir.com/upload/resizer2/10/f71/f7186234317f1c991842337949d3e345.JPG",
+                        "categoryId": 3
+                    },
+                    {
+                        "name": "Не заставляйте меня думать",
+                        "authorName": "Стив Круг",
+                        "price": 1169,
+                        "coverUrl": "https://avatars.mds.yandex.net/get-mpic/4331935/img_id1438814894880962860.jpeg/orig",
+                        "categoryId": 1
+                    },
+                    {
+                        "name": "Как пасти котов",
+                        "authorName": "Дж.Ханк Рейнвотер",
+                        "price": 1207,
+                        "coverUrl": "https://cdn1.ozone.ru/multimedia/1024486505.jpg",
+                        "categoryId": 2
+                    },
+                    {
+                        "name": "Пять пороков команды",
+                        "authorName": "Патрик Ленсиони",
+                        "price": 1649,
+                        "coverUrl": "https://www.combook.ru/imgrab/0073/9785001951865.jpg",
+                        "categoryId": 2
+                    },
+                    {
+                        "name": "Совершенный код",
+                        "authorName": "Стив Макконнелл",
+                        "price": 1787,
+                        "coverUrl": "https://cdn1.ozone.ru/multimedia/1020973362.jpg",
+                        "categoryId": 3
+                    },
+                    {
+                        "name": "Scrum. Революционный метод управления проектами",
+                        "authorName": "Джефф Сазерленд",
+                        "price": 1937,
+                        "coverUrl": "https://bookprose.ru/pictures/1016752146.jpg",
+                        "categoryId": 2
+                    },
+                    {
+                        "name": "Модульные системы в графическом дизайне",
+                        "authorName": "Йозеф Мюллер-Брокманн",
+                        "price": 2048,
+                        "coverUrl": "https://cdn1.ozone.ru/multimedia/1023267350.jpg",
+                        "categoryId": 1
+                    },
+                    {
+                        "name": "Deadline. Роман об управлении проектами",
+                        "authorName": "Том Демарко",
+                        "price": 2448,
+                        "coverUrl": "https://main-cdn.sbermegamarket.ru/big2/hlr-system/174826385/100024719491b1.jpg",
+                        "categoryId": 2
+                    },
+                    {
+                        "name": "Код: тайный язык информатики",
+                        "authorName": "Чарльз Петцольд",
+                        "price": 2479,
+                        "coverUrl": "https://khanin.info/uploads/20140921/code.jpg",
+                        "categoryId": 3
+                    },
+                    {
+                        "name": "Программист-прагматик",
+                        "authorName": "Дэвид Томас, Эндрю Хант",
+                        "price": 3495,
+                        "coverUrl": "https://cdn1.ozone.ru/multimedia/1013930687.jpg",
+                        "categoryId": 3
+                    },
+                    {
+                        "name": "Шаблоны корпоративных приложений",
+                        "authorName": "Мартин Фаулер",
+                        "price": 4295,
+                        "coverUrl": "https://assets.htmlacademy.ru/content/blog/377/3@1x.jpg",
+                        "categoryId": 3
+                    },
+                    {
+                        "name": "Рефакторинг. Улучшение проекта, существующего кода",
+                        "authorName": "Мартин Фаулер",
+                        "price": 7125,
+                        "coverUrl": "https://www.ukazka.ru/img/g/uk272988.jpg",
+                        "categoryId": 3
+                    },
+                    {
+                        "name": "Алгоритмы. Построение и анализ",
+                        "authorName": "Томас Кормен, Чарльз Лейзерсон, Рональд Ривест, Клиффорд Штайн",
+                        "price": 16875,
+                        "coverUrl": "https://cdn1.ozone.ru/multimedia/1026535643.jpg",
+                        "categoryId": 3
+                    }
+                ]
             })
         }
+
         this.setLoading(false)
     }
 
@@ -273,12 +414,34 @@ class Store {
     categories = []
 
     fetchCategories = async () => {
-        const petReq = await fetch(`${host}/bookstore-api/books/categories`);
-        const petRes = await petReq.json();
-        // console.log(petRes);
-        if (petReq.ok) {
+        try {
+            const petReq = await fetch(`${host}/bookstore-api/books/categories`);
+            const petRes = await petReq.json();
+            if (petReq.ok) {
+                runInAction(() => {
+                    this.categories = petRes
+                })
+            }
+        } catch {
             runInAction(() => {
-                this.categories = petRes
+                this.categories = [
+                    {
+                        "id": 1,
+                        "name": "Дизайн"
+                    },
+                    {
+                        "id": 2,
+                        "name": "Управление проектами"
+                    },
+                    {
+                        "id": 3,
+                        "name": "Разработка"
+                    },
+                    {
+                        "id": 4,
+                        "name": "Тестирование"
+                    }
+                ]
             })
         }
     }
